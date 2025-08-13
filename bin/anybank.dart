dae5bb67b42d4d93c01327a54fb7223c64b409c8
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-08-13 14:46:40 -0300

    inheritance and polymorphism

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -3,6 +3,8 @@ import 'package:my_app/conta.dart';
 void main() {
   Conta contaMatheus = Conta("Matheus", 1000);
   Conta contaFelipe = Conta("Felipe", 2000);
+  ContaCorrente contaLari = ContaCorrente("Lari", 1000);
+  ContaPoupanca contaVand = ContaPoupanca("Vand", 1000);
 
   List<Conta> contas = <Conta>[contaMatheus, contaFelipe];
 
@@ -12,5 +14,12 @@ void main() {
 
   contaFelipe.receber(200);
   contaFelipe.enviar(300);
+
+  contaLari.imprimiSaldo();
+  contaLari.enviar(1300);
+
+  contaVand.imprimiSaldo();
+  contaVand.calculaRendimento();
+  contaVand.imprimiSaldo();
 }
 
