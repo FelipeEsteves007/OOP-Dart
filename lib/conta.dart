502562487a8d5002108d25bcfbc2d86ae6071289
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-08-13 14:47:27 -0300

    inheritance and polymorphism

--- lib/conta.dart
+++ lib/conta.dart
@@ -20,3 +20,26 @@ class Conta {
     print("O saldo atual de $titular é: R\$$_saldo");
   }
 }
+
+class ContaCorrente extends Conta {  // herança
+  double emprestimo = 300;
+  ContaCorrente(super.titular, super._saldo);
+
+  @override // sobrescrita
+  void enviar(double valor) {
+    if (_saldo + emprestimo >= valor ){
+      _saldo -= valor;
+      imprimiSaldo();
+    }
+  }
+}
+
+class ContaPoupanca extends Conta {
+  double rendimento = 0.05;
+  
+  ContaPoupanca(super.titular, super._saldo);
+
+  void calculaRendimento (){
+    _saldo +=  _saldo  * rendimento;
+  }
+}
