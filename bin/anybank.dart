5536e10c7e87b86c36811434833d05d741854951
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-08-13 17:30:42 -0300

    complete work

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,25 +1,14 @@
 import 'package:my_app/conta.dart';
 
 void main() {
-  Conta contaMatheus = Conta("Matheus", 1000);
-  Conta contaFelipe = Conta("Felipe", 2000);
+
   ContaCorrente contaLari = ContaCorrente("Lari", 1000);
   ContaPoupanca contaVand = ContaPoupanca("Vand", 1000);
+  ContaEmpresa contaFe = ContaEmpresa("FEFE", 1000);
+  ContaInvestimento contaLipe = ContaInvestimento("LIPE", 2000);
 
-  List<Conta> contas = <Conta>[contaMatheus, contaFelipe];
+  contaFe.receber(1000);
+  contaLipe.enviar(1000);
 
-  for (Conta conta in contas) {
-    conta.imprimiSaldo();
-  }
-
-  contaFelipe.receber(200);
-  contaFelipe.enviar(300);
-
-  contaLari.imprimiSaldo();
-  contaLari.enviar(1300);
-
-  contaVand.imprimiSaldo();
-  contaVand.calculaRendimento();
-  contaVand.imprimiSaldo();
 }
 
--- lib/conta.dart
+++ lib/conta.dart
@@ -1,33 +1,34 @@
-class Conta {
+abstract class Conta {
   String titular;
   double _saldo;
 
   Conta(this.titular, this._saldo); // função construtora
 
-  void receber(double valor) {  
+  void receber(double valor) {
     _saldo += valor;
     imprimiSaldo();
   }
 
   void enviar(double valor) {
-    if (_saldo >= valor){
-     _saldo -= valor;
-     imprimiSaldo();
+    if (_saldo >= valor) {
+      _saldo -= valor;
+      imprimiSaldo();
     }
   }
 
-  void imprimiSaldo (){
+  void imprimiSaldo() {
     print("O saldo atual de $titular é: R\$$_saldo");
   }
 }
 
-class ContaCorrente extends Conta {  // herança
+class ContaCorrente extends Conta {
+  // herança
   double emprestimo = 300;
   ContaCorrente(super.titular, super._saldo);
 
   @override // sobrescrita
   void enviar(double valor) {
-    if (_saldo + emprestimo >= valor ){
+    if (_saldo + emprestimo >= valor) {
       _saldo -= valor;
       imprimiSaldo();
     }
@@ -36,10 +37,56 @@ class ContaCorrente extends Conta {  // herança
 
 class ContaPoupanca extends Conta {
   double rendimento = 0.05;
-  
+
   ContaPoupanca(super.titular, super._saldo);
 
-  void calculaRendimento (){
-    _saldo +=  _saldo  * rendimento;
+  void calculaRendimento() {
+    _saldo += _saldo * rendimento;
   }
+}
+
+mixin Imposto {
+  double taxa = 0.03;
+
+  double valorTaxado(double valor) {
+    return valor * taxa;
+  }
+}
+
+class ContaEmpresa extends Conta with Imposto {
+  ContaEmpresa(super.titular, super._saldo);
+
+  @override
+  void enviar(double valor) {
+    if (_saldo >= valor + valorTaxado(valor)) {
+      _saldo -= valor + valorTaxado(valor);
+        imprimiSaldo();
+    }
+  }
+
+  @override
+  void receber(double valor) {
+    _saldo += valor - valorTaxado(valor);
+      imprimiSaldo();
+  }
+}
+
+
+class ContaInvestimento extends Conta with Imposto {
+  ContaInvestimento(super.titular, super._saldo);
+
+  @override
+  void enviar (double valor){
+    if (_saldo >= valor + valorTaxado(valor)){
+      _saldo -= valor + valorTaxado(valor);
+        imprimiSaldo();
+    }
+  }
+
+  @override 
+  void receber (double valor){
+      _saldo += valor - valorTaxado(valor);
+        imprimiSaldo();
+  }
+  
 }
